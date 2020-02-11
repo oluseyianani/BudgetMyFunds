88ab32a03a56d49b506f5cd47aceb8638cd93650
Adegoke Fred <fredrick.adegoke@gmail.com>
2020-02-11 14:52:15 +0100

    Revert "#171206889 Bug fix testing sqlite"

--- app/User.php
+++ app/User.php
@@ -1,23 +1,22 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-use Illuminate\Support\Str;
-use App\Notifications\VerifyApiEmail;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'email', 'phone_number', 'password',
+        'name', 'email', 'password',
     ];
 
     /**
@@ -37,21 +36,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-
-    /**
-     *
-     */
-    public function generateToken()
-    {
-        $this->api_token = Str::random(60);
-        $this->save();
-
-        return $this;
-    }
-
-    public function sendApiEmailVerificationNotification()
-    {
-        $this->notify(new VerifyApiEmail);
-    }
 }
