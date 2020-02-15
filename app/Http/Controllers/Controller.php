dde67e9d2f7e5609298052a8d7ff45d4ce6f4498
Oluseyi Anani <anasey@outlook.com>
2020-02-16 00:44:24 +0100

    Adds Swagger Documentation [finishes #171034863]

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,18 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+/**
+ * @SWG\Swagger(
+ *     basePath="/api/v1",
+ *     schemes={"http", "https"},
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="BudgetMyFunds API",
+ *         description="BudgetMyFunds App API Documentation"
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
