89ba76892bc7daba1da1989cc0a67be56cf31151
Oluseyi Anani <anasey@outlook.com>
2020-02-16 00:44:24 +0100

    Adds Swagger Documentation [finishes #171034863]

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -42,10 +42,59 @@ class RegisterController extends Controller
     }
 
     /**
-     * Get a validator for an incoming registration request.
+     * @SWG\Post(
+     *   path="/auth/register",
+     *   description="Registers a user",
+     *   consumes={"multipart/form-data", "application/json"},
+     *       @SWG\Parameter(
+     *          name="email",
+     *          description="user's email",
+     *          required=true,
+     *          type="string",
+     *          in="body",
+     *          @SWG\Schema(
+     *              @SWG\Property(property="email", type="string", example="john.doe@email.com")
+     *         )
+     *      ),
+     *      @SWG\Parameter(
+     *          name="phone_number",
+     *          description="The user's phone number",
+     *          required=false,
+     *          type="string",
+     *          in="body",
+     *          @SWG\Schema(
+     *              @SWG\Property(property="phone_number", type="string", example="+2348011111111111")
+     *         )
+     *      ),
+     *      @SWG\Parameter(
+     *          name="password",
+     *          description="Users password. Minimum eight characters, at least one letter and one number",
+     *          required=true,
+     *          type="integer",
+     *          in="body",
+     *          @SWG\Schema(
+     *              @SWG\Property(property="password", type="string", example="password123")
+     *         )
+     *      ),
+     *      @SWG\Parameter(
+     *          name="password_confirmation",
+     *          description="Users confirm password. Minimum eight characters, at least one letter and one number",
+     *          required=true,
+     *          type="string",
+     *          in="body",
+     *          @SWG\Schema(
+     *              @SWG\Property(property="password_confirmation", type="string", example="password123")
+     *         )
+     *      ),
      *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
+     *   @SWG\Response(response=422, description="Invalid email or passowrd"),
+     *   @SWG\Response(response=201, description="Registered"),
+     * )
+     *
+     * Registers a new user
+     * @param App\Http\Request\CreateUserRequest $request
+     * @return json
+     *
      */
     protected function validator(array $data)
     {
